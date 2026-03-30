1e7046c9970668935dbeee7399cdda2ca03838cc
Julien Van Roy <julien.vanroy@macq.eu>
2026-03-30 11:41:15 +0200

    OO Basics: exercises

--- src/main/scala/com/rockthejvm/part2oop/OOBasics.scala
+++ src/main/scala/com/rockthejvm/part2oop/OOBasics.scala
@@ -35,5 +35,49 @@ object OOBasics {
   def main(args: Array[String]): Unit = {
     println(johnSayHiToDaniel)
     println(aPerson.greet())
+
+    val charlesDickens = new Writer("Charles", "Dickens", 1812)
+    val charlesDickensImpostor = new Writer("Charles", "Dickens", 2021)
+    val novel = new Novel("Great Expectations", 1861, charlesDickens)
+    val newEdition = novel.copy(1871)
+
+    println(charlesDickens.fullName)
+    println(novel.authorAge)
+    println(novel.isWrittenBy(charlesDickens))
+    println(novel.isWrittenBy(charlesDickensImpostor))
+    println(newEdition.authorAge)
   }
+}
+
+class Writer(firstName: String, lastName: String, val yearOfBirth: Int) {
+  def fullName: String = firstName + " " + lastName
+}
+
+class Novel(title: String, yearOfRelease: Int, author: Writer) {
+  def authorAge: Int = this.yearOfRelease - author.yearOfBirth
+  def isWrittenBy(author: Writer): Boolean = this.author == author
+  def copy(newYear: Int): Novel = new Novel(title, newYear, author)
+}
+
+class Counter(count: Int = 0) {
+  def increment(): Counter = {
+    new Counter(count + 1)
+  }
+
+  def decrement(): Counter =
+    if (count == 0) this
+    else new Counter(count - 1)
+
+  def increment(n: Int): Counter = {
+    if (n <= 0) this
+    else increment().increment(n - 1)  // vulnerable to stack overflows
+  }
+
+  def decrement(n: Int): Counter = {
+    if (n <= 0) this
+    else decrement().decrement(n - 1)
+  }
+
+  def print(): Unit =
+    println(s"Current count: $count")
 }
